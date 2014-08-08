67ebd7978ca0ec5c42a58335caf563294f38a01c
Kevin Gut <kevin.gut@bitm.sg>
2014-08-08 23:39:12 +0200

    adding and designing form

---
+++ AOTP/frmMain.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace AOTP
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbFiles = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rbEnc = new System.Windows.Forms.RadioButton();
+            this.rbDec = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OFD = new System.Windows.Forms.OpenFileDialog();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.pbFile = new System.Windows.Forms.ProgressBar();
+            this.pbTotal = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.FBD = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // lbFiles
+            // 
+            this.lbFiles.AllowDrop = true;
+            this.lbFiles.FormattingEnabled = true;
+            this.lbFiles.Location = new System.Drawing.Point(12, 49);
+            this.lbFiles.Name = "lbFiles";
+            this.lbFiles.Size = new System.Drawing.Size(365, 173);
+            this.lbFiles.TabIndex = 3;
+            this.lbFiles.DragDrop += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragDrop);
+            this.lbFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.lbFiles_DragEnter);
+            this.lbFiles.KeyDown += new System.Windows.Forms.KeyEventHandler(this.lbFiles_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "1. Select files to add to the list or drag them onto it";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(404, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "2. Order the list with the up and down buttons so the first to be released file i" +
+                "s on top.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "3. Select the operation";
+            // 
+            // rbEnc
+            // 
+            this.rbEnc.AutoSize = true;
+            this.rbEnc.Checked = true;
+            this.rbEnc.Location = new System.Drawing.Point(12, 250);
+            this.rbEnc.Name = "rbEnc";
+            this.rbEnc.Size = new System.Drawing.Size(156, 17);
+            this.rbEnc.TabIndex = 7;
+            this.rbEnc.TabStop = true;
+            this.rbEnc.Text = "Encrypt and generate a key";
+            this.rbEnc.UseVisualStyleBackColor = true;
+            // 
+            // rbDec
+            // 
+            this.rbDec.AutoSize = true;
+            this.rbDec.Location = new System.Drawing.Point(12, 272);
+            this.rbDec.Name = "rbDec";
+            this.rbDec.Size = new System.Drawing.Size(267, 17);
+            this.rbDec.TabIndex = 8;
+            this.rbDec.Text = "Decrypt and assume the key is the last file in the list";
+            this.rbDec.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "4.";
+            // 
+            // OFD
+            // 
+            this.OFD.Filter = "All Files|*.*";
+            this.OFD.Multiselect = true;
+            this.OFD.Title = "Select source file";
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(31, 295);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(75, 23);
+            this.btnGo.TabIndex = 10;
+            this.btnGo.Text = "Go...";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // pbFile
+            // 
+            this.pbFile.Location = new System.Drawing.Point(6, 350);
+            this.pbFile.Name = "pbFile";
+            this.pbFile.Size = new System.Drawing.Size(407, 23);
+            this.pbFile.TabIndex = 12;
+            // 
+            // pbTotal
+            // 
+            this.pbTotal.Location = new System.Drawing.Point(6, 379);
+            this.pbTotal.Name = "pbTotal";
+            this.pbTotal.Size = new System.Drawing.Size(407, 23);
+            this.pbTotal.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(9, 330);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(70, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "5. Just watch";
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Image = global::AOTP.Properties.Resources.del;
+            this.btnRemove.Location = new System.Drawing.Point(383, 85);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(30, 30);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Image = global::AOTP.Properties.Resources.add;
+            this.btnAdd.Location = new System.Drawing.Point(383, 49);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(30, 30);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Image = global::AOTP.Properties.Resources.down;
+            this.btnDown.Location = new System.Drawing.Point(383, 192);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(30, 30);
+            this.btnDown.TabIndex = 5;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Image = global::AOTP.Properties.Resources.up;
+            this.btnUp.Location = new System.Drawing.Point(383, 156);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(30, 30);
+            this.btnUp.TabIndex = 4;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // FBD
+            // 
+            this.FBD.Description = "Select destination folder";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 412);
+            this.Controls.Add(this.pbTotal);
+            this.Controls.Add(this.pbFile);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.rbDec);
+            this.Controls.Add(this.rbEnc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "AOTP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rbEnc;
+        private System.Windows.Forms.RadioButton rbDec;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.OpenFileDialog OFD;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.ProgressBar pbFile;
+        private System.Windows.Forms.ProgressBar pbTotal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.FolderBrowserDialog FBD;
+    }
+}
+
---
+++ AOTP/frmMain.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace AOTP
+{
+    public partial class frmMain : Form
+    {
+        private BackgroundWorker BW;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            BW = new BackgroundWorker();
+            BW.WorkerReportsProgress = true;
+
+            BW.DoWork += new DoWorkEventHandler(BW_DoWork);
+            BW.RunWorkerCompleted += new RunWorkerCompletedEventHandler(BW_RunWorkerCompleted);
+            BW.ProgressChanged += new ProgressChangedEventHandler(BW_ProgressChanged);
+        }
+
+        void BW_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            pbTotal.Value = e.ProgressPercentage;
+        }
+
+        void BW_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            ena(true);
+            pbFile.Value = pbTotal.Value = 0;
+            MessageBox.Show("Operation completed");
+        }
+
+        void BW_DoWork(object sender, DoWorkEventArgs e)
+        {
+            pbTotal.Value = 0;
+            if (rbEnc.Checked)
+            {
+                encrypt(e.Argument.ToString());
+            }
+            else
+            {
+                decrypt(e.Argument.ToString());
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            addFiles();
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            removeSelected();
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            moveUp();
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            moveDown();
+        }
+
+        private void lbFiles_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                e.SuppressKeyPress = e.Handled = true;
+                removeSelected();
+            }
+            else if (e.KeyCode == Keys.Insert)
+            {
+                e.SuppressKeyPress = e.Handled = true;
+                addFiles();
+            }
+            else if (e.Modifiers == Keys.Control)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    e.SuppressKeyPress = e.Handled = true;
+                    moveUp();
+                }
+                else if (e.KeyCode == Keys.Down)
+                {
+                    e.SuppressKeyPress = e.Handled = true;
+                    moveDown();
+                }
+            }
+        }
+
+        private void addFiles()
+        {
+            if (OFD.ShowDialog() == DialogResult.OK)
+            {
+                lbFiles.Items.AddRange(OFD.FileNames);
+            }
+        }
+
+        private void removeSelected()
+        {
+            if (lbFiles.SelectedIndex >= 0)
+            {
+                int index = lbFiles.SelectedIndex;
+                lbFiles.Items.RemoveAt(index);
+                if (index < lbFiles.Items.Count)
+                {
+                    lbFiles.SelectedIndex = index;
+                }
+            }
+        }
+
+        private void moveDown()
+        {
+            int index = lbFiles.SelectedIndex;
+            if (index >= 0 && index < lbFiles.Items.Count - 1)
+            {
+                lbFiles.Items.Insert(index + 2, lbFiles.Items[index]);
+                lbFiles.Items.RemoveAt(index);
+                lbFiles.SelectedIndex = index + 1;
+            }
+        }
+
+        private void moveUp()
+        {
+            int index = lbFiles.SelectedIndex;
+            if (index > 0 && index < lbFiles.Items.Count)
+            {
+                lbFiles.Items.Insert(index - 1, lbFiles.Items[index]);
+                lbFiles.Items.RemoveAt(index + 1);
+                lbFiles.SelectedIndex = index - 1;
+            }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (!BW.IsBusy)
+            {
+
+                if (FBD.ShowDialog() == DialogResult.OK)
+                {
+                    ena(false);
+                    BW.RunWorkerAsync(FBD.SelectedPath);
+                }
+            }
+            else
+            {
+                ena(false);
+            }
+        }
+
+        private void encrypt(string path)
+        {
+            xorProgressHandler xPH = new xorProgressHandler(xorProgress);
+
+            OTP.xorProgress += xPH;
+
+            string[] Files = new string[lbFiles.Items.Count];
+            int i;
+            for (i = 0; i < Files.Length; i++)
+            {
+                Files[i] = lbFiles.Items[i].ToString();
+            }
+
+            //encrypt
+            long longest = 0L;
+
+            foreach (string s in Files)
+            {
+                FileInfo F = new FileInfo(s);
+                if (F.Length == 0)
+                {
+                    MessageBox.Show("Error on file " + F.FullName + "\r\n\r\nYou cannot encrypt 0 byte files.\r\n(You technically could but it is totally mean)\r\n\r\nOperation is aborted", "0-byte file detected", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                else if (F.Length + (long)OTP.generateHeader(F.Name, F.Length).Length > longest)
+                {
+                    longest = F.Length + (long)OTP.generateHeader(F.Name, F.Length).Length;
+                }
+            }
+
+            //ready to encrypt. Generate key
+            using (var keyOut = File.Create(path + "\\key.bin"))
+            {
+                OTP.generateKey(longest, OTP.RandomSeed, keyOut);
+                BW.ReportProgress(100 / (Files.Length + 1));
+            }
+
+            i = 0;
+
+            foreach (string s in Files)
+            {
+                if (i == 0)
+                {
+                    using (var keyIn = File.OpenRead(path + "\\key.bin"))
+                    {
+                        using (var FileOut = File.Create(string.Format("{0}\\enc_{1}.bin", path, Files.Length - i)))
+                        {
+                            OTP.xorFile(s, keyIn, FileOut);
+                        }
+                    }
+                }
+                else
+                {
+                    //encrypt using previously encrypted file
+                    using (var keyIn = File.OpenRead(string.Format("{0}\\enc_{1}.bin", FBD.SelectedPath, Files.Length - i + 1)))
+                    {
+                        using (var FileOut = File.Create(string.Format("{0}\\enc_{1}.bin", FBD.SelectedPath, Files.Length - i)))
+                        {
+                            OTP.xorFile(s, keyIn, FileOut);
+                        }
+                    }
+                }
+                i++;
+                BW.ReportProgress(i * 100 / (Files.Length + 1));
+            }
+
+            OTP.xorProgress -= xPH;
+
+            BW.ReportProgress(100);
+        }
+
+        private void decrypt(string path)
+        {
+            xorProgressHandler xPH = new xorProgressHandler(xorProgress);
+
+            OTP.xorProgress += xPH;
+
+            string[] Files = new string[lbFiles.Items.Count];
+            for (int i = 0; i < Files.Length; i++)
+            {
+                Files[i] = lbFiles.Items[i].ToString();
+            }
+
+            //decrypt
+
+            //this loop does not decrypts the last file as it is assumed to be key only
+            for (int i = 0; i < Files.Length - 1; i++)
+            {
+                using (var InFile = File.OpenRead(Files[i]))
+                {
+                    using (var InKey = File.OpenRead(Files[i + 1]))
+                    {
+                        OTP.HeaderProps HP = OTP.decryptHeader(InFile, InKey);
+
+                        if (HP.FileLength >= 0)
+                        {
+
+                            using (var OutFile = File.Create(path + "\\" + HP.FileName))
+                            {
+                                using (var RangeIn = new RangedStream(InFile, 0, HP.FileLength))
+                                {
+                                    using (var RangeKey = new RangedStream(InKey, 0, HP.FileLength))
+                                    {
+                                        OTP.xor(RangeIn, RangeKey, OutFile);
+                                        BW.ReportProgress((i + 1) * 100 / Files.Length);
+                                    }
+                                }
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show("Error decoding File " + Files[i] + "\r\nProbably wrong key.", "Operation aborted");
+                            break;
+                        }
+                    }
+                }
+            }
+            OTP.xorProgress -= xPH;
+            BW.ReportProgress(100);
+        }
+
+        private void xorProgress(int i)
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    xorProgress(i);
+                });
+            }
+            else
+            {
+                pbFile.Value = i;
+            }
+        }
+
+        private void ena(bool p)
+        {
+            foreach (Control c in this.Controls)
+            {
+                if (c is Button || c is ListBox || c is RadioButton)
+                {
+                    c.Enabled = p;
+                }
+            }
+        }
+
+        private void lbFiles_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void lbFiles_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            lbFiles.Items.AddRange(files);
+        }
+    }
+}
